64ae17004586204ee6e1ddb9c5f592c32cbabc47
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-09-29 17:44:07 +0200

    changing path

--- Job8/get_logs.sh
+++ Job8/get_logs.sh
@@ -4,12 +4,13 @@ d=$(date +"%e-%m-%Y-%H-%M")
 file="number_connection-$d"
 last | grep "yann" | wc -l > $file
 
-mkdir -p /home/yann/Scripts/BashHWork/Job8/Backup
+mkdir -p /home/yann/Scripts/BashHWork/BashHWork/Job8/Backup
 
-tar -cf $file.tar /home/yann/Scripts/BashHWork/Job8/Backup
+tar -cf  /home/yann/Scripts/BashHWork/BashHWork/Job8/$file.tar $file
 
-mv $file $file.tar /home/yann/Scripts/BashHWork/Job8/Backup
+mv /home/yann/Scripts/BashHWork/BashHWork/Job8/$file.tar /home/yann/Scripts/BashHWork/BashHWork/Job8/Backup
 
+rm $file
 
 
 
--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -3,21 +3,22 @@
 
 sed 1,1d /home/yann/Scripts/BashHWork/BashHWork/Job9/Shell_Userlist.csv | cat | while read line
 do
-	for (( i=1 ; i<=5 ; i++ ))
+	for (( i=1 ; i<6 ; i++ ))
 	do
-		((a$i=$(echo $line | cut -d "," -f $i)))
+		((a${i}=$(echo ${line%?} | cut -d "," -f $i )))
+		echo "$a5"
 	done
 
-		if [ $a5 = "User" ]
-		then
-			useradd -u $a1 "$a2.$a3"
-			echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
-		elif [ $a5 = "Admin" ]
-		then
-			useradd -u $a1 "$a2.$a3"
-			echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
-			echo "$a2.$a3 ALL=(ALL:ALL) ALL" >> /etc/sudoers
-		else
-			echo "erreur"
-		fi
+	if [ "$a5" = "User" ]
+	then
+		useradd -u $a1 "$a2.$a3"
+		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
+	elif [ "$a5" = "Admin" ]
+	then
+		useradd -u $a1 "$a2.$a3"
+		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
+		echo "$a2.$a3 ALL=(ALL:ALL) ALL" >> /etc/sudoers
+	else
+		echo "erreur"
+	fi
 done
