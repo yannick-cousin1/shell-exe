293a933afc2e8550fff2de26adc98f6cdf964bd1
yannick-cousin1 <yannick.cousin@laplateforme.io>
2022-09-30 10:00:54 +0200

    finished Job9

--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-
 sed 1,1d /home/yann/Scripts/BashHWork/BashHWork/Job9/Shell_Userlist.csv | cat | while read line
-do
-	for (( i=1 ; i<6 ; i++ ))
-	do
-		((a${i}=$(echo ${line%?} | cut -d "," -f $i )))
-		echo "$a5"
-	done
 
-	if [ "$a5" = "User" ]
+do
+
+	a1=$(echo "$line" | cut -d "," -f 1)
+	a2=$(echo "$line" | cut -d "," -f 2)
+	a3=$(echo "$line" | cut -d "," -f 3)
+	a4=$(echo "$line" | cut -d "," -f 4)
+	a5=$(echo "$line" | cut -d "," -f 5)
+#	echo $a5
+
+	if [ "${a5%?}" = "User" ]
 	then
-		useradd -u $a1 "$a2.$a3"
-		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
-	elif [ "$a5" = "Admin" ]
+		useradd -u "$a1" -m "${a2// /}${a3// /}"
+		echo "${a2// /}${a3// /}:${a4// /}" | chpasswd
+
+	elif [ "${a5%?}" = "Admin" ] 
 	then
-		useradd -u $a1 "$a2.$a3"
-		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
-		echo "$a2.$a3 ALL=(ALL:ALL) ALL" >> /etc/sudoers
+		useradd -u "$a1" -m "${a2// /}${a3// /}"
+		echo "${a2// /}${a3// /}:${a4// /}" | chpasswd
+		adduser "${a2// /}${a3// /}" sudo
 	else
-		echo "erreur"
+		echo "incorrect line"
 	fi
 done
+
---
+++ Job9/draftAccesRights.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+sed 1,1d /home/yann/Scripts/BashHWork/BashHWork/Job9/Shell_Userlist.csv | cat | while read line
+do
+	for (( i=1 ; i<6 ; i++ ))
+	do
+		((a${i}=$(echo ${line%?} | cut -d "," -f $i )))
+		echo "${a5}"
+	done
+
+	if [ "$a5" = "User" ]
+	then
+		useradd -u $a1 "$a2.$a3"
+		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
+	elif [ "$a5" = "Admin" ]
+	then
+		useradd -u $a1 "$a2.$a3"
+		echo -e "$a4\n$a4\n" | passwd "$a2.$a3"
+		echo "$a2.$a3 ALL=(ALL:ALL) ALL" >> /etc/sudoers
+	else
+		echo "erreur"
+	fi
+done
